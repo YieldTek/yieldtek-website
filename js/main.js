55158fd7a22a3fe2e9b9d920fe98f17a016ca606
Sean Moriarty <sean.moriarty@gopassport.com>
2017-11-23 23:52:17 -0500

    Pushing up the new site

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#demo-link').click(function () {
+       window.location.href = 'https://www.twitch.tv/hightekgrow';
+    });
+    $('#store-link').click(function () {
+        window.location.href = 'http://yieldtek.com/store.html';
+    });
+    $('#blog-link').click(function () {
+        window.location.href = 'http://ttcubicle.blogspot.com/';
+    });
+
+    $('#wiki-link').click(function () {
+        window.location.href = 'http://wiki.hightekco.com/';
+    });
+});
